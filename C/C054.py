3ceb251be0e3964779da5c49650455c5d299f644
yamato-creator <sioonigiri5656@outlook.jp>
2021-03-05 21:57:56 +0900

    C054

---
+++ C/C054.py
@@ -0,0 +1,12 @@
+N, velocity = map(int,input().split())
+result = "NO"
+s = [list(input().split()) for _ in range(N)]
+# print(s)
+
+for i in range(len(s)):
+    if int(s[i][0]) == 0 or int(s[i][1]) == 0:
+        continue
+    elif velocity <= (int(s[i][0]) / int(s[i][1])):
+        result = "YES"
+        
+print(result)
